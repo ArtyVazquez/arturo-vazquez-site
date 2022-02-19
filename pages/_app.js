75a0b53f81e7d9adaa02e6ea5959174266c68fc8
Arturo Vazquez <arrttttyyyyyy@yahoo.com>
2022-02-19 08:25:40 -0600

    layout done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,94 @@
+import { useState, createContext, useMemo, useContext, useEffect } from 'react';
+import ResponsiveDrawer from '../Components/NavbarDrawer/ResponsiveDrawer'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import { useCookies } from 'react-cookie';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+// Component that will toggle light/dark mode themes...
+function ThemeMode() {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+  return (      
+      <ListItemIcon  onClick={() => colorMode.toggleColorMode(true)} color="inherit">
+        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      </ListItemIcon>
+  );
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  const [cookies, setCookie] = useCookies(['theme']); // save theme mode in cookie
+  const [mode, setMode] = useState('light'); // light mode by default
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: (isToogle) => {
+        if (isToogle){
+          setCookie('theme', cookies.theme === 'light'? 'dark':'light');
+          setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+        } else {
+          setMode(cookies.theme);
+        }
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  // set the theme to be whats in the theme cookie
+  useEffect(() => {
+    if (cookies.theme === undefined)
+      setCookie('theme', 'light', {path: '/'});
+
+    colorMode.toggleColorMode(false);
+  },[]);
+
+  return (
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+         
+          <Box 
+            sx={{ 
+              display: 'flex',
+              flexDirection: 'row',
+              heigth: '100vh',
+              overflowX: 'hidden'
+             }}>
+            <ResponsiveDrawer mode={<ThemeMode />}/>
+            
+            {/* make 100% of heigth... */}
+            <Box
+             sx={{
+              bgcolor: 'background.default',
+              color: 'text.primary',
+              minHeight: '100vh',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+              }}>
+              <Component {...pageProps} />
+            </Box>
+
+          </Box>
+
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+  )
+}
+
+export default MyApp;
